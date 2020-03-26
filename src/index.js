a604a453432b4b772ed5cbf3e423f6da4d6ad1e4
Mike Boring <shekinah999@msn.com>
2020-03-26 06:42:23 -0600

    Added to scroll to top component for scroll to top for view transitions

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { ConnectedRouter } from "connected-react-router";
 import { App } from "./react";
 import { store, history } from "./redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ScrollToTop from "react-router-scroll-top";
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
